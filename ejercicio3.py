5f6427055d0d54cfc92f931ae54a98d383a0c7b0
enriquemacias21 <enriquemacias21@hotmail.com>
2024-03-09 12:35:10 -0500

    Actualizacion ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,10 @@
 class Circulo:
     def __init__(self, radio):
         # Asigna el radio proporcionado al atributo 'radio'
+        self.radio=radio
 
     def calcular_area(self):
         return 3.14 * self.radio ** 2
+
+circulo = Circulo(3)
+print(circulo.calcular_area())

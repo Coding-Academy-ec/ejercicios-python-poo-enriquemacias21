f929d49efdaf33a365a396dfa2889d7787e04c06
enriquemacias21 <enriquemacias21@hotmail.com>
2024-03-09 12:31:16 -0500

    Actualización del ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,7 @@
 class Animal:
     def __init__(self, nombre):
         # Asigna el nombre proporcionado al atributo 'nombre'
+        self.nombre=nombre
 
     def hacer_sonido(self):
         pass
@@ -8,7 +9,14 @@ class Animal:
 class Perro(Animal):
     def hacer_sonido(self):
         # Devuelve el sonido de un perro (ladrido)
+        return "¡Guau!"
 
 class Gato(Animal):
     def hacer_sonido(self):
         # Devuelve el sonido de un gato (maullido)
+        return "¡Miau!"
+perro = Perro("Bobby")
+print("El perro se llama: ", perro.nombre," y hace: ", perro.hacer_sonido())
+
+gato = Gato("Garfield")
+print("El gato se llama: ", gato.nombre," y hace: ", gato.hacer_sonido())

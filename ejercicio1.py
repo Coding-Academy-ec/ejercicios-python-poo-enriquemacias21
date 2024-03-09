29c52d790025e3fa11801d2e014c9a255e52e4b9
enriquemacias21 <enriquemacias21@hotmail.com>
2024-03-09 12:18:57 -0500

    Actualizacion de ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,13 @@
 class Coche:
     def __init__(self, marca, modelo):
         # Asigna la marca proporcionada al atributo 'marca'
+        self.marca="Toyota"
         # Asigna el modelo proporcionado al atributo 'modelo'
+        self.modelo="Corolla"
 
     def conducir(self):
         return f"Conduciendo el {self.marca} {self.modelo}"
+
+coche = Coche('Toyota', 'Corolla')
+print(coche.conducir())
 
